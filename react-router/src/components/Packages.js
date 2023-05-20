357b0af1527c11255daf33c9322b2a22c7b166f6
Colin <colindcirian@gmail.com>
2023-05-20 11:44:06 -0700

    first commit

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,16 @@
 
 
-export default function Packages() {
-
+export default function Packages( {packages} ) {
+    const displayPackages = packages.map(p => <li>{ p }</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {displayPackages}
+                </ul>
             </div>
         </div>
     )
